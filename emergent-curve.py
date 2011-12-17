738c27e650b4e5e02728c4a730e24df5f33fd55c
Matt Ball <yosemitebandit@gmail.com>
2011-12-16 20:01:21 -0800

    renamed and better org

--- emergent-curve.py
+++ emergent-curve.py
@@ -1,22 +1,28 @@
+'''
+emergent-curve.py
+draws straight lines between points on the x and y axes to form curves
+usage, 10cm axes with a step of 1/5cm:
+    $ python emergent-curve.py 10 5
+'''
 import sys
 
 import plotter
 
 p = plotter.Plotter(
-        # hard to measure solo
+        # motor separation..still not quite sure this is right
         160.97
-        # height doesn't really matter
-        #, 3*12*2.54
+        # height is arbitrary but seems to affect the skew of things
         , 160.97
-        # caliper-measured
+        # caliper-measured motor radius
         , 1.375
+        # path to my controller
         , '/dev/serial/by-id/usb-SchmalzHaus_EiBotBoard-if00')
 
 units = int(sys.argv[1])
+scaling = float(sys.argv[2])
+
 p.move_to_relative([0, units])
 p.move_to_relative([units, 0])
-
-scaling = float(sys.argv[2])
 
 counter = 0
 while(1):
