18b898523ca217fdb09ecd93de246311c9cf7403
Matt Ball <yosemitebandit@gmail.com>
2013-02-06 19:49:38 -0800

    adds a new script used at the last demo

---
+++ bin/move-self.py
@@ -0,0 +1,35 @@
+'''
+move.py
+move the pen up, down, left and right
+usage, move down 5cm: 
+    $ python move.py 5 d
+'''
+import sys
+
+import erik
+
+p = erik.Plotter(
+        # hard to measure solo
+        #160.97
+        159.5
+        # height doesn't really matter
+        #, 3*12*2.54
+        , 160.97
+        # caliper-measured
+        , 1.375
+        , speed=3
+        , serial_path='/dev/serial/by-id/usb-SchmalzHaus_EiBotBoard-if00')
+
+move_distance = float(sys.argv[1])
+direction = sys.argv[2]
+position = p.position
+
+# adjust the desired x,y coordinates of the pen head                   
+if direction == 'd':                                                   
+    p.move_to_relative([0, -move_distance])
+elif direction == 'u':                                                 
+    p.move_to_relative([0, move_distance])
+elif direction == 'r':                                                 
+    p.move_to_relative([move_distance, 0])
+elif direction == 'l':                                                 
+    p.move_to_relative([-move_distance, 0])
